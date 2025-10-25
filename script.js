31d7ff1b43d01ed6190054cb7130704aa2bb6f90
ultrmarin <martin.onga@ivkhk.ee>
2025-10-25 20:05:35 +0300

    first skill + mana

--- script.js
+++ script.js
@@ -82,22 +82,22 @@ let textRoundCount = document.getElementById("text-round-count")
 let roundCount = 1
 
 const gesture_wins = {
-    "rock": ["fire","scissors","snake","person","tree","wolf","sponge"],
-    "fire": ["scissors","snake","person","tree","wolf","sponge","paper"],
-    "scissors": ["snake","person","tree","wolf","sponge","paper","air"],
-    "snake": ["person","tree","wolf","sponge","paper","air","water"],
-    "person": ["tree","wolf","sponge","paper","air","water","boom"],
-    "tree": ["wolf","sponge","paper","air","water","boom","dragon"],
-    "wolf": ["sponge","paper","air","water","boom","dragon","devil"],
-    "sponge": ["paper","air","water","boom","dragon","devil","lightning"],
-    "paper": ["air","water","boom","dragon","devil","lightning","pistol"],
-    "air": ["water","boom","dragon","devil","lightning","pistol","rock"],
-    "water": ["boom","dragon","devil","lightning","pistol","rock","fire"],
-    "boom": ["dragon","devil","lightning","pistol","rock","fire","scissors"],
-    "dragon": ["devil","lightning","pistol","rock","fire","scissors","snake"],
-    "devil": ["lightning","pistol","rock","fire","scissors","snake","person"],
-    "lightning": ["pistol","rock","fire","scissors","snake","person","tree"],
-    "pistol": ["rock","fire","scissors","snake","person","tree","wolf"],
+    "rock": ["fire","scissors","snake","person","tree","wolf","sponge",  "group1"],
+    "fire": ["scissors","snake","person","tree","wolf","sponge","paper",  "group1"],
+    "scissors": ["snake","person","tree","wolf","sponge","paper","air",  "group1"],
+    "snake": ["person","tree","wolf","sponge","paper","air","water",  "group1"],
+    "person": ["tree","wolf","sponge","paper","air","water","boom",  "group2"],
+    "tree": ["wolf","sponge","paper","air","water","boom","dragon",  "group2"],
+    "wolf": ["sponge","paper","air","water","boom","dragon","devil",  "group2"],
+    "sponge": ["paper","air","water","boom","dragon","devil","lightning",  "group2"],
+    "paper": ["air","water","boom","dragon","devil","lightning","pistol",  "group3"],
+    "air": ["water","boom","dragon","devil","lightning","pistol","rock",  "group3"],
+    "water": ["boom","dragon","devil","lightning","pistol","rock","fire",  "group3"],
+    "boom": ["dragon","devil","lightning","pistol","rock","fire","scissors",  "group3"],
+    "dragon": ["devil","lightning","pistol","rock","fire","scissors","snake",  "group4"],
+    "devil": ["lightning","pistol","rock","fire","scissors","snake","person",  "group4"],
+    "lightning": ["pistol","rock","fire","scissors","snake","person","tree",  "group4"],
+    "pistol": ["rock","fire","scissors","snake","person","tree","wolf",  "group4"]
 }
 
 const firstHp = document.getElementById("first-hp")
@@ -127,12 +127,12 @@ gestureAllButtons.forEach(button => {
 });
 
 
-function botChooseGesture() {
-    const gestures = Object.keys(gesture_wins);
-    // матх рандом выдаёт рандом число от 0 до 1,потом перемножаем на всю длинну списка жестов и округляем - выходит типа псевдо-рандом жеста
-    const random = Math.floor(Math.random() * gestures.length);
-    return gestures[random];
-}
+// function botChooseGesture() {
+//     const gestures = Object.keys(gesture_wins);
+//     // матх рандом выдаёт рандом число от 0 до 1,потом перемножаем на всю длинну списка жестов и округляем - выходит типа псевдо-рандом жеста
+//     const random = Math.floor(Math.random() * gestures.length);
+//     return gestures[random];
+// }
 
 // если вызывать каждый раз функции по новой,то жест постояннл разный будет и это всё ломает
 let botChooseGesture1
@@ -146,8 +146,10 @@ let botChooseGesture1
 
 confirmBtn.addEventListener('click', () => {
     if(playerGestureChoice != "-") {
+        roundMana()
         textTimerRound.innerHTML = "TIME: 5 s"
         clearInterval(intervalTimer)
+        textTimer.innerHTML = "TIME: 60 s"
         if (roundScreen.style.display == "none"){
             roundScreen.style.display = 'flex';
             endRoundTimer()
@@ -166,12 +168,30 @@ confirmBtn.addEventListener('click', () => {
             }
         }
     }
-    // нужно сделать уведомление того,что нужно выбрать жест для подтверждени
+    // нужно сделать уведомление того,что нужно выбрать жест для подтверждени по типу alert или pop окна
 });
 
 // простейшая функция таймера,если таймер заканчивается,а игрок ничё не выбрал - пикает камень
 function roundTimer(){
-    botChooseGesture1 = botChooseGesture()
+    if (selectSkill == 1){
+        botChooseGesture1 = botChooseGesture("group1")
+        selectSkill = 0
+        console.log("Virubili 1")
+    } else if(selectSkill == 2){
+        botChooseGesture1 = botChooseGesture("group2")
+        selectSkill = 0
+        console.log("Virubili 2")
+    } else if(selectSkill == 3){
+        botChooseGesture1 = botChooseGesture("group3")
+        selectSkill = 0
+        console.log("Virubili 3")
+    } else if(selectSkill == 4){
+        botChooseGesture1 = botChooseGesture("group4")
+        selectSkill = 0
+        console.log("Virubili 4")
+    } else{
+        botChooseGesture1 = botChooseGesture()
+    }
     // console.log(botChooseGesture1)
     textRoundCount.innerHTML = roundCount;
     let defaultTimer = 60
@@ -293,3 +313,87 @@ function botHpLose(){
 //     console.log(testtimer-=1)
 // }, "1000")
 
+
+// всё что связано с маной
+const divManaCount = document.getElementById("div-mana-count")
+const textManaCount = document.getElementById("text-mana-count")
+
+let playerManaCount = 2;
+let divManaHeight = 450;
+
+
+// вынеc всю проверку маны в отдельную функцию,чтоб не срать в остальных
+function calcMana(){
+    divManaHeight = 450 - (playerManaCount*50)
+    divManaCount.style.height = divManaHeight + "px"
+    divManaCount.style.display = "flex"
+    if (playerManaCount >= 10){
+        playerManaCount = 10
+        divManaCount.style.display = "none"
+    }
+    textManaCount.innerHTML = playerManaCount + " mana"
+    if (playerManaCount == 0){
+        textManaCount.style.display = "none"
+    } else{
+        textManaCount.style.display = "flex"
+    }
+}
+
+function roundMana(){
+    if (playerManaCount < 10){
+        playerManaCount+= 2
+    }
+    calcMana()
+}
+
+// пикает все кнопочки с классом этим,а потом я вызываю их через инлекс,так удобнее я глаголю
+const skillsBtn = document.querySelectorAll(".skills-btn")
+
+let selectSkill = 0
+
+skillsBtn[0].addEventListener("click", () => {
+    if (playerManaCount >= 2 && selectSkill == 0){
+        playerManaCount-= 2
+        selectSkill = 1
+    }
+    calcMana()
+})
+
+skillsBtn[1].addEventListener("click", () => {
+    if (playerManaCount >= 2 && selectSkill == 0){
+        playerManaCount-= 2
+        selectSkill = 2
+    }
+    calcMana()
+})
+
+skillsBtn[2].addEventListener("click", () => {
+    if (playerManaCount >= 2 && selectSkill == 0){
+        playerManaCount-= 2
+        selectSkill = 3
+    }
+    
+    calcMana()
+})
+
+skillsBtn[3].addEventListener("click", () => {
+    if (playerManaCount >= 2 && selectSkill == 0){
+        playerManaCount-= 2
+        selectSkill = 4
+    }
+    calcMana()
+})
+
+// тот же самый рандомный выбор бота,только теперь учитывает выключение одной из групп
+function botChooseGesture(Group) {
+    const gestures = Object.keys(gesture_wins)
+    // тотальный ужас,короче фильтрует весь лист от 1 группы и бот пикает жесты без неё
+    // gesture это мы обращаемся в html файл к аттрибуту дата-гестур
+    const availGestures = gestures.filter(gesture => {
+        const all_gesture = gesture_wins[gesture]
+        const all_group = all_gesture[all_gesture.length - 1]
+        return all_group != Group
+    });
+    const random = Math.floor(Math.random() * availGestures.length)
+    return availGestures[random]
+}
